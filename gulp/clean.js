6a3a5d4e94f09f3e626e92204f84e401032d05d1
Nick Waelkens <hello@nickwaelkens.be>
2016-02-07 17:08:40 +0100

    Get rid of gulp tasks and their dependencies

--- gulp/clean.js
+++
@@ -1,8 +0,0 @@
-import config from './config';
-import del from 'del';
-
-export default (gulp) => {
-    gulp.task('clean', function () {
-        return del(`${config.dist.root}/**/*`);
-    });
-};
--- gulp/default.js
+++
@@ -1,9 +0,0 @@
-import runSequence from 'run-sequence';
-
-export default (gulp) => {
-    runSequence.use(gulp);
-
-    gulp.task('default', () => {
-        runSequence('clean', ['zip']);
-    });
-};
