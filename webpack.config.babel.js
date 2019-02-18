c3b11c66a492d1f964187b25f263c3687eb7aa52
Nick Waelkens <hi@nick.plus>
2019-02-19 00:45:39 +0100

    :sparkles: Modernize setup

---
+++ webpack.config.babel.js
@@ -0,0 +1,51 @@
+import webpack from 'webpack';
+import LodashModuleReplacementPlugin from 'lodash-webpack-plugin';
+import path from 'path';
+
+const resolvePath = (...args) => path.resolve(path.resolve(__dirname), ...args);
+const resolveClientPath = (pathName = '') => resolvePath('src', pathName);
+
+export default (environment = 'development') => {
+  const __DEV__ = environment === 'development';
+  const APP_ENTRY = [resolveClientPath('index.js')];
+
+  const webpackConfig = {
+    entry: {
+      app: ['@babel/polyfill', ...APP_ENTRY],
+    },
+    output: {
+      filename: '[name].js',
+      path: resolvePath('dist'),
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          include: [resolveClientPath()],
+          use: ['babel-loader'],
+        },
+      ],
+    },
+    devtool: __DEV__ ? 'inline-source-map' : false,
+    optimization: {
+      namedModules: __DEV__,
+      splitChunks: {
+        cacheGroups: {
+          appVendors: {
+            name: 'vendors',
+            test: /[\\/]node_modules[\\/]/,
+            chunks: chunk => chunk.name === 'app',
+          },
+        },
+      },
+      runtimeChunk: {
+        name: 'manifest',
+      },
+    },
+    plugins: [
+      new LodashModuleReplacementPlugin(),
+    ],
+  };
+
+  return webpackConfig;
+};
