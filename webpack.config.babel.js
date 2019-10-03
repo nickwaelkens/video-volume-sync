0becf03ed6bd5c3c9edc0b1e0cde1f8fc1164baa
Nick Waelkens <hi@nick.plus>
2019-10-03 15:53:11 +0200

    :wrench: Dynamically create manifest

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -4,41 +4,39 @@ const resolvePath = (...args) => path.resolve(path.resolve(__dirname), ...args);
 const resolveClientPath = (pathName = '') => resolvePath('src', pathName);
 const APP_ENTRY = resolveClientPath('index.js');
 
-export default (environment = 'development') => {
-  const __DEV__ = environment === 'development';
+const __DEV__ = process.env.NODE_ENV === 'development';
 
-  return {
-    entry: {
-      app: APP_ENTRY,
-    },
-    output: {
-      filename: '[name].js',
-      path: resolvePath('dist'),
-    },
-    module: {
-      rules: [
-        {
-          test: /\.js$/,
-          include: [resolveClientPath()],
-          use: ['babel-loader'],
-        },
-      ],
-    },
-    devtool: __DEV__ ? 'inline-source-map' : false,
-    optimization: {
-      namedModules: __DEV__,
-      splitChunks: {
-        cacheGroups: {
-          appVendors: {
-            name: 'vendors',
-            test: /[\\/]node_modules[\\/]/,
-            chunks: chunk => chunk.name === 'app',
-          },
+export default {
+  entry: {
+    app: APP_ENTRY,
+  },
+  output: {
+    filename: '[name].js',
+    path: resolvePath('dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        include: [resolveClientPath()],
+        use: ['babel-loader'],
+      },
+    ],
+  },
+  devtool: __DEV__ ? 'inline-source-map' : false,
+  optimization: {
+    namedModules: __DEV__,
+    splitChunks: {
+      cacheGroups: {
+        appVendors: {
+          name: 'vendors',
+          test: /[\\/]node_modules[\\/]/,
+          chunks: chunk => chunk.name === 'app',
         },
       },
-      runtimeChunk: {
-        name: 'manifest',
-      },
     },
-  };
+    runtimeChunk: {
+      name: 'manifest',
+    },
+  },
 };
