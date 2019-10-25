b56c135db3a86820deaf84ed68acb86354a825b0
Nick Waelkens <hi@nick.plus>
2019-10-25 11:00:22 +0200

    :bug: Use MutationObserver

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,10 @@ import chromeStorage from './chrome-storage';
 import debounce from 'lodash.debounce';
 
 let desiredVolume = 0.5;
+let videos:Array<HTMLVideoElement> = [];
 
 const updateVideos = () => {
-  [...document.getElementsByTagName('video')].forEach(video => {
+  videos.forEach(video => {
     video.volume = desiredVolume;
   });
 };
@@ -22,8 +23,27 @@ document.addEventListener('volumechange', event => {
   }
 }, true);
 
+const observer = new MutationObserver(mutations => {
+  mutations.forEach(mutation => {
+    if (mutation.type === 'childList' && mutation.addedNodes.length > 0) {
+      for (const node of mutation.addedNodes) {
+        // Only track elements, skip other nodes (e.g. text nodes)
+        if (!(node instanceof HTMLElement)) continue;
+
+        for (const video of node.getElementsByTagName('video')) {
+          video.muted = desiredVolume === 0;
+          video.volume = desiredVolume;
+          videos = [...new Set([...videos, video])];
+        }
+      }
+    }
+  });
+});
+
 (async function main() {
   desiredVolume = await Number(chromeStorage.get('desiredVolume')) || 0.5;
+  observer.observe(document, { childList: true, subtree: true });
 
+  videos = [...document.getElementsByTagName('video')];
   updateVideos();
 }());
